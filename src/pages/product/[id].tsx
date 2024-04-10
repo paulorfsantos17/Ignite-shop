e994606c9d53756ac4e64e894bfba0af9121ba26
Paulo <paulorfsantos17@gmail.com>
2024-04-10 13:56:59 -0300

    fix: Rename component page products

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 
-export default function Product() {
+export default function ProductPage() {
   const { query } = useRouter()
   return <div>Product {JSON.stringify(query)}</div>
 }
