69a807e8aedc4aed0020184bbe7607c2a66a0f99
Paulo <paulorfsantos17@gmail.com>
2024-04-11 15:52:16 -0300

    feat: add create static page success

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,21 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
 import { ImageContainer, SuccessContainer } from '@/styles/pages/success'
 
 export default function Success() {
-  return
-  ;<SuccessContainer>
-    <h1>Compra Efetuada!</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra Efetuada!</h1>
 
-    <ImageContainer></ImageContainer>
+      <ImageContainer></ImageContainer>
 
-    <p>
-      Uhuul <strong>Diego Fernandes</strong>, sua <strong>Camiseta nome da camisa</strong> já está a caminho de sua
-      casa./
-    </p
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua{' '}
+        <strong>Camiseta nome da camisa</strong> já está a caminho de sua casa.
+      </p>
 
-    <Link href='/'>
-      Voltar
-    </Link>
-  </SuccessContainer>
+      <Link href="/">Voltar ao Catálogo</Link>
+    </SuccessContainer>
+  )
 }
